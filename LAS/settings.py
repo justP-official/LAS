0c94b96c8b909c1f7f332c387ef67932d7a01499
justP-official <red-hot-shot@yandex.ru>
2024-08-21 13:54:20 +0300

    dev - Создание dev-ветки

--- LAS/settings.py
+++ LAS/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
