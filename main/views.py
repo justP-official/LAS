9f84810f6cc4275f19056016dda04e75ed87920b
justP-official <red-hot-shot@yandex.ru>
2024-09-05 19:57:46 +0300

    Небольшие улучшения - Изменено отображение форм; Добавлены документ-строки для представлений;

--- main/views.py
+++ main/views.py
@@ -1,19 +1,32 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from user.forms import UserLoginForm
+
+
 def view_404(request, exception):
+    """Функция представления для обработки ошибки 404"""
     context = {'title': 'L.A.S - 404'}
     return render(request, '404.html', context)
 
 def view_403(request, exception):
+    """Функция представления для обработки ошибки 403"""
     context = {'title': 'L.A.S - 403'}
     return render(request, '403.html', context)
 
 def view_500(request):
+    """Функция представления для обработки ошибки 500"""
     context = {'title': 'L.A.S - 500'}
     return render(request, '500.html', context)
 
 
 class IndexView(TemplateView):
+    """Класс представления для отображения главной страницы"""
     template_name = 'main/index.html'
-    extra_context = {'title': 'L.A.S - Главная'}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UserLoginForm()
+        context['title'] = 'L.A.S - Главная'
+        return context
--- user/forms.py
+++ user/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField()
-    password = forms.CharField()
+    username = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
 
     class Meta:
         model = get_user_model()
@@ -13,7 +13,7 @@ class UserLoginForm(AuthenticationForm):
 
 
 class UserPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField()
-    new_password1 = forms.CharField()
-    new_password2 = forms.CharField()
+    old_password = forms.CharField(label='Старый пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Старый пароль'}))
+    new_password1 = forms.CharField(label='Новый пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Новый пароль'}))
+    new_password2 = forms.CharField(label='Подтверди пароль', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Подтверди пароль'}))
     
